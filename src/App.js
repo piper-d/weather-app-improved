a778703dccdfac3e2a80f1a32e80901636aa3b9d
Dylan Piper <dylan_piper@knights.ucf.edu>
2021-03-31 19:58:37 -0400

    Add city and weather fetches

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import './App.css';
 import { Button, TextField, Box } from "@material-ui/core";
 import { useState } from "react";
 
+const searchURL = "https://www.metaweather.com/api/location/search/?query=";
+const weatherURL = "https://www.metaweather.com/api/location/";
+
 function App() {
   const [text, setText] = useState("");
+
+  async function fetchCityData() {
+    const data = await fetch(searchURL + text).then((blob) => blob.json());
+    const city = data[0];
+    const woeid = city.woeid;
+    const weatherData = await fetch(weatherURL + woeid).then((blob) => blob.json());
+  }
   return (
     <Box m={3}>
       <TextField 
       value={text} 
       onChange={(event) => setText(event.target.value)} />
-      <Button variant="contained" color="primary">Click me!</Button>
+      <Button variant="contained" color="primary" onClick={fetchCityData} >Click me!</Button>
     </Box>
   );
 }
