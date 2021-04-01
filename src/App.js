d625394c0c4098eb3d5bc1fe92b3684034fccdec
Dylan Piper <dylan_piper@knights.ucf.edu>
2021-03-31 20:04:22 -0400

    Add Card component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Button, TextField, Box } from "@material-ui/core";
+import { Button, TextField, Box, Card, CardContent } from "@material-ui/core";
 import { useState } from "react";
 
 const searchURL = "https://www.metaweather.com/api/location/search/?query=";
@@ -7,12 +7,15 @@ const weatherURL = "https://www.metaweather.com/api/location/";
 
 function App() {
   const [text, setText] = useState("");
+  const [weather, setWeather] = useState("");
 
   async function fetchCityData() {
     const data = await fetch(searchURL + text).then((blob) => blob.json());
     const city = data[0];
     const woeid = city.woeid;
     const weatherData = await fetch(weatherURL + woeid).then((blob) => blob.json());
+    const weather = weatherData.consolidated_weather[0].weather_state_name;
+    setWeather(weather);
   }
   return (
     <Box m={3}>
@@ -20,6 +23,11 @@ function App() {
       value={text} 
       onChange={(event) => setText(event.target.value)} />
       <Button variant="contained" color="primary" onClick={fetchCityData} >Click me!</Button>
+      <Card>
+        <CardContent>
+          {weather}
+        </CardContent>
+      </Card>
     </Box>
   );
 }
